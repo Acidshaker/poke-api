af2307f32974f346aa014a0c411bff0b4a48b2b5
Jorge Ortegón <jorge_barcelino@hotmail.com>
2023-01-01 19:18:29 -0600

    first commit

---
+++ src/components/PokemonOptions.jsx
@@ -0,0 +1,66 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const PokemonOptions = ({ currentNamePokemon, setCurrentNamePokemon }) => {
+  const [pokemons, setPokemons] = useState();
+
+  useEffect(() => {
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+
+    if (currentNamePokemon) {
+      axios
+        .get(URL)
+        .then((res) => {
+          if (
+            res.data.results.filter(
+              (pokemon) =>
+                pokemon.name.includes(currentNamePokemon) &&
+                pokemon.name !== currentNamePokemon
+            )
+          ) {
+            setPokemons(
+              res.data.results.filter((pokemon) =>
+                pokemon.name.includes(currentNamePokemon)
+              )
+            );
+          }
+          // else if (
+          //   res.data.results.filter(
+          //     (pokemon) => pokemon.name === currentNamePokemon
+          //   )
+          // ) {
+          //   setPokemons([]);
+          // }
+          else {
+            setPokemons(res.data.results);
+          }
+        })
+        .catch((err) => console.log(err));
+    } else {
+      setPokemons([]);
+    }
+  }, [currentNamePokemon]);
+
+  const handleClickPokemonOption = (currentName) => {
+    setCurrentNamePokemon(currentName);
+    setPokemons([]);
+  };
+
+  return (
+    <ul className="pokemon__option--list">
+      {pokemons?.map((pokemon) => (
+        <li
+          onClick={() => {
+            handleClickPokemonOption(pokemon?.name);
+          }}
+          key={pokemon?.url}
+          className="pokemon__option--item"
+        >
+          {pokemon?.name}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default PokemonOptions;
